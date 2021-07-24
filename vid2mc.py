6d8192b768f2de0997c49fd565b363e0a44d52d1
jasmaa <jasonmaa3955@gmail.com>
2021-07-24 10:44:45 -0400

    Read fps from capture

--- vid2mc.py
+++ vid2mc.py
@@ -131,8 +131,9 @@ if __name__ == '__main__':
                 w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
             if not h:
                 h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+            fps = cap.get(cv2.CAP_PROP_FPS)
             fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-            out = cv2.VideoWriter(out_fname, fourcc, 20.0, (w, h))
+            out = cv2.VideoWriter(out_fname, fourcc, fps, (w, h))
         while cap.isOpened():
             ret, frame = cap.read()
             if not ret:
